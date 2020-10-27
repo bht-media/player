05c0beb4d7b3c828f9b0613cd1a1199ec247a038
unknown <mkkehrer@googlemail.com>
2020-10-27 12:31:36 +0100

    changed titel #3

--- channel.js
+++ channel.js
@@ -310,7 +310,7 @@ function getChannelConfig(config) {
     title = 'Test Stream / ABR';
   } else if (channel == 'test-abr-3') {
     config.source = config_ABR_1;
-    title = 'Vitalparametermonitoring in Kirgisistan';
+    title = 'EPI - Exoskelette nach dem Prinzip elastischer Insektenlokomotion';
   } else if (channel == 'asta-wahl-2020') {
     config.source.h5live.rtmp.streamname = 'RBJ00-FzI9V';
     title = 'AStA Wahl 2020';
