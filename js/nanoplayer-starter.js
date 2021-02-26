4adab3765c065bab917db0a4e19232f1855f3042
MKKehrer <mkkehrer@googlemail.com>
2021-02-26 16:05:49 +0100

    updated logic

--- js/nanoplayer-starter.js
+++ js/nanoplayer-starter.js
@@ -93,7 +93,7 @@ function startPlayerSimpleConfig(playerDiv, stream) {
     let titleId = document.getElementById('title2');
     if(titleId&&stream.title) titleId.innerText = stream.title;
     // set Brückenkurs-Button visibility
-    let buttonBB = (stream.tag.indexOf("ckenkurs")>=0);
+    let buttonBB = (stream.indexOf("ckenkurs")>=0);
     let bbId = document.getElementById('bb_link');
     let bbIdInfo = document.getElementById('bb_link_info');
     let bbIdbb = document.getElementById('bb_bb');
